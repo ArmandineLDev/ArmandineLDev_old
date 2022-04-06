5d2f3652fd49e7ed7141f0f19d686feb277ea139
Armandine337711 <armandine.barge@ntymail.com>
2022-04-06 10:47:11 +0200

    index.js update import createRoot

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM, { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
   <BrowserRouter>
     <App />
