0a10f3620527040969c588623b83fe2666ff77a8
Armandine BARGE <armandine.barge@ntymail.com>
2022-07-05 17:32:52 +0200

    create layout

---
+++ client-ald/components/Layout/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <h1>This is body</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
